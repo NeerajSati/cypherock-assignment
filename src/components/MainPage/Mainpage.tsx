c4120f30c9cddae31d2c5da8a145fde88c3824bd
Neeraj Sati <neeraj.sati123@gmail.com>
2022-06-11 17:12:13 +0530

    Added Filters and topbar in Main Page

--- src/components/MainPage/Mainpage.tsx
+++ src/components/MainPage/Mainpage.tsx
@@ -1,8 +1,41 @@
 import React from "react";
+import "./mainpage.css";
+import lock from "../../images/lock.svg";
+import darkmode from "../../images/darkmode.svg";
+import synchronized from "../../images/synchronized.svg";
+import filter from "../../images/filter.svg";
+import drop from "../../images/drop.svg";
+import sort from "../../images/sort.svg";
 function Mainpage() {
   return (
     <div className="mainpage">
-      MainPage
+      <div className="topbar">
+        <div className="synchronized">
+          <img src={synchronized} alt="synchronized"></img>
+          Synchronized
+        </div>
+        <button className="topbarBtn">
+          <img src={darkmode} alt="darkmode"></img>
+        </button>
+        <button className="topbarBtn">
+          <img src={lock} alt="lock"></img>
+        </button>
+      </div>
+      <div className="walletDetails">
+        <h1 className="walletName">Wallet 1</h1>
+        <button className="addCoin">+ Add Coin</button>
+      </div>
+      <div className="coinFiltersWrapper">
+        <p className="coinCount">Total Coins-3</p>
+        <div className="coinFilters">
+        <div className="coinFilter">
+          <img src={filter} alt="filter"></img><span className="filter">Year</span>
+          <img src={drop} alt="drop"></img></div>
+        <div className="coinFilter">
+          <img src={sort} alt="sort"></img><span className="filter">A-Z</span>
+          <img src={drop} alt="drop"></img></div>
+        </div>
+      </div>
     </div>
   );
 }
